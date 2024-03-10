294358424b70867bb3a79daf3afcee6d0b7ded18
diptanshu mandal <diptanshu.mandal@go-yubi.com>
2024-03-10 19:38:05 +0530

    initial commit

---
+++ src/main/java/com/example/quickStart/QuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //identifies it as spring boot application and also a starting point of springBoot
+public class QuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartApplication.class, args);
+	}
+
+}
